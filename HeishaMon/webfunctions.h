4ecd1293bd78f4796f68b35c2456e51909f7b1de
Krzysztof Białek <kbialek@users.noreply.github.com>
2023-12-30 19:29:33 +0100

    Update mqtt topic base

--- HeishaMon/webfunctions.h
+++ HeishaMon/webfunctions.h
@@ -9,8 +9,7 @@ struct settingsStruct {
   uint16_t updateAllTime = 300; // how often all data is resend to mqtt
 
   char mqtt_port[6] = "1883";
-  char mqtt_topic_base[128] = "panasonic_heat_pump";
-  char mqtt_topic_listen[128] = "master_panasonic_heat_pump";
+  char mqtt_topic_base[128] = "home/hvac/heatpump/board";
 
   bool listenonly = true; //listen only so heishamon can be installed parallel to cz-taw1, set commands will not work though
   bool optionalPCB = false; //do we emulate an optional PCB?
