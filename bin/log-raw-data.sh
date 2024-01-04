0d95a9af4f17b45b5045e6190dc593bc2e71840d
Krzysztof Białek <kbialek@users.noreply.github.com>
2024-01-04 21:45:58 +0100

    Fix raw log printing

--- bin/log-raw-data.sh
+++ bin/log-raw-data.sh
@@ -2,10 +2,4 @@
 
 set -uf -o pipefail
 
-function read_raw_data_line() {
-    mosquitto_sub -h "$MQTT_SERVER" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -F '%t : %x' -C 1 -t 'home/hvac/heatpump/board/raw/#'
-}
-
-while true; do
-    echo $(read_raw_data_line)
-done
+mosquitto_sub -h "$MQTT_SERVER" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -F '%t : %x' -t 'home/hvac/heatpump/board/raw/#'
